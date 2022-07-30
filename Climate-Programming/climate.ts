60959c4c37aab3c2e24274b1f31019a9c7f11520
Cryptolibertarian-id <hazekaizer@gmail.com>
2022-07-30 15:04:27 +0700

    add green house effect

--- Climate-Programming/climate.ts
+++ Climate-Programming/climate.ts
@@ -41,8 +41,36 @@ class Disease {
   }
 }
 
+class GreenHouseEffect {
+  increaseSeaLevelDamage: number = 200;
+  lossWildLifeSpeciesDamage: number = 200;
+  stormsDamage: number = 250;
+  hurricaneDamage: number = 250;
+
+  public get increaseSeaLevel(): number {
+    return this.increaseSeaLevelDamage;
+  }
+
+  public set increaseSeaLevel(v: number) {
+    this.increaseSeaLevelDamage = v;
+  }
+
+  public get lossWildLifeSpecies(): number {
+    return this.lossWildLifeSpeciesDamage;
+  }
+
+  public set lossWildLifeSpecies(v: number) {
+    this.lossWildLifeSpeciesDamage = v;
+  }
+
+  public get stormsDamage(): number {
+    return this.stormsDamage;
+  }
+}
+
 class WildFire {
   heatWaveDamage: number = 250;
+  animalMigration: number = 100;
 
   public get heatWave(): number {
     return this.heatWaveDamage;
