7f08f9e1a8e5a07e877b6d5ae36f59d1a638f1a4
Cryptolibertarian-id <hazekaizer@gmail.com>
2022-07-31 01:17:56 +0700

    add type keywoed

--- Types/type-aliases.ts
+++ Types/type-aliases.ts
@@ -1,3 +1,18 @@
+type Balance = number;
+type Type = string;
+
+type Wallet = {
+  name: string;
+  amount: Balance;
+  symbol: Type;
+};
+
+let bitcoinWallet: Wallet = {
+  name: "Bitcoin",
+  amount: 5.88800007,
+  symbol: "BTC",
+};
+
 type Point = {
   x: number;
   y: number;
