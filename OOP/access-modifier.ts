10dbe50a355f5d08cbbf9b724ebbcfb838d11141
Cryptolibertarian-id <hazekaizer@gmail.com>
2022-07-30 15:48:33 +0700

    add protected

--- OOP/access-modifier.ts
+++ OOP/access-modifier.ts
@@ -8,7 +8,20 @@ class Warrior {
     return this.attack * 2;
   }
 
+  protected trippleAttack() {
+    this.stamina -= 10;
+    return this.attack * 3;
+  }
+
+  bash() {
+    console.log("Bash!");
+
+    return this.trippleAttack();
+  }
+
   public get myStamina(): number {
+    console.log("My Stamina");
+
     return this.stamina;
   }
 
@@ -24,3 +37,25 @@ console.log(knight.defence);
 console.log(knight.myStamina);
 console.log(knight.doubleAttack());
 console.log(knight.myStamina);
+/* console.log(knight.trippleAttack());
+Property 'trippleAttack' is protected and only accessible within class 'Warrior' and its subclasses. */
+console.log(knight.bash());
+
+class Assasin extends Warrior {
+  private flee: number = 3;
+  constructor() {
+    super();
+    this.defence += 100;
+    this.myStamina += 100;
+  }
+
+  public doTrippleAttack() {
+    return this.trippleAttack();
+  }
+}
+
+let jonathan = new Assasin();
+console.log(jonathan.myStamina);
+console.log(jonathan.doTrippleAttack());
+console.log(jonathan.myStamina);
+console.log(jonathan.defence);
