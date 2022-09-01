6b404fdebbaaa8346b926c5399ad009bbfa4bbc0
Cryptolibertarian-id <hazekaizer@gmail.com>
2022-09-01 12:40:04 +0700

    add find last index of

--- src/array/find-lastIndex-element.ts
+++ src/array/find-lastIndex-element.ts
@@ -1,3 +1,12 @@
 var arrayString: string[] = ["Banana", "Apple", "Strawberry", "Apple"];
-const foundArrStr = arrayString.findLastIndex((element) => element === "Apple");
-console.log(foundArrStr); //3
+console.time("Find Last Index");
+const foundArrStr: number = arrayString.findLastIndex(
+  (element) => element === "Apple"
+);
+console.log(foundArrStr); //1
+console.timeEnd("Find Last Index");
+
+console.time("Last Index Of");
+const resLastIndexOf: number = arrayString.lastIndexOf("Apple");
+console.log(resLastIndexOf);
+console.timeEnd("Last Index Of");
