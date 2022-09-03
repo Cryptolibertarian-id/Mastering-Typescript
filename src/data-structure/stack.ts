da77e66a85cd6240bc3d289ed0cb7ead03253211
Cryptolibertarian-id <hazekaizer@gmail.com>
2022-09-04 02:14:59 +0700

    add stack

--- src/data-structure/stack.ts
+++ src/data-structure/stack.ts
@@ -1,10 +1,29 @@
 class Stack {
-  items: string[];
+  #items: string[];
   constructor() {
-    this.items = [];
+    this.#items = [];
+  }
+  push(element: string) {
+    this.#items.push(element);
+  }
+  pop() {
+    this.#items.pop();
+  }
+  peek() {
+    return this.#items[this.#items.length - 1];
+  }
+  isEmpty() {
+    return this.size() === 0;
+  }
+  size() {
+    return this.#items.length;
+  }
+  clear() {
+    this.#items = [];
   }
 }
 
 const ostack = new Stack();
-ostack.items.push("Hello World!");
+console.log(ostack.isEmpty());
+ostack.push("Hello World!");
 console.log(ostack);
