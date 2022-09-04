37b48fea61d1b739abd7427927fe14cb1c646709
Cryptolibertarian-id <hazekaizer@gmail.com>
2022-09-04 16:52:28 +0700

    add push pop

---
+++ src/array/pop.ts
@@ -0,0 +1,7 @@
+var arrayStr: string[] = [];
+arrayStr.push("Bitcoin");
+arrayStr.push("Satoshi");
+arrayStr.push("Nakamoto");
+arrayStr.push("Blockchain");
+arrayStr.pop();
+console.log(arrayStr.length); //3
---
+++ src/array/push.ts
@@ -0,0 +1,6 @@
+var arrayStr: string[] = [];
+arrayStr.push("Bitcoin");
+arrayStr.push("Satoshi");
+arrayStr.push("Nakamoto");
+arrayStr.push("Blockchain");
+console.log(arrayStr.length); //4
