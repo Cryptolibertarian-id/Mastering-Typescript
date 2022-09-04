c84056cd91b99ce944dac43a3a0a67bc0672ad56
Cryptolibertarian-id <hazekaizer@gmail.com>
2022-09-04 23:01:27 +0700

    add type alias

---
+++ src/object/type-alias.ts
@@ -0,0 +1,11 @@
+type TBlockchain = {
+  name: string;
+  creator: string;
+  totalCoin: number;
+};
+
+const Type_bitcoin: TBlockchain = {
+  name: "Bitcoin",
+  creator: "Satoshi Nakamoto",
+  totalCoin: 21000000,
+};
