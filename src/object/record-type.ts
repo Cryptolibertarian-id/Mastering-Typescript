1fb45617745049ae2614baea1fd24e1c5142f6df
Cryptolibertarian-id <hazekaizer@gmail.com>
2022-09-04 23:56:03 +0700

    add record type

---
+++ src/object/record-type.ts
@@ -0,0 +1,6 @@
+const agent: Record<string, any> = {};
+agent.name = "Gun";
+agent.age = false;
+agent.skill = ["DevOps", "Blockchain"];
+
+console.log(agent); //{ name: 'Gun', age: 30, skill: [ 'DevOps', 'Blockchain' ] }
