34d0f0f7eae75f4324250f813bd01fab40e4ac4b
Cryptolibertarian-id <hazekaizer@gmail.com>
2022-09-11 02:34:40 +0700

    add arrow function

---
+++ src/function/arrow-function-multiline.ts
@@ -0,0 +1,7 @@
+let summary = (a: number, b: number): number => {
+  // curly brace to create multiline function
+  let result = a + b;
+  return result; // use return keyword
+};
+
+console.log(summary(1, 2)); // 3
---
+++ src/function/arrow-function.ts
@@ -0,0 +1,10 @@
+let sum = (a: number, b: number) => a + b;
+
+/* Equal with:
+
+  let sum = function(a, b) {
+  return a + b;
+  };
+  */
+
+console.log(sum(1, 2)); // 3
